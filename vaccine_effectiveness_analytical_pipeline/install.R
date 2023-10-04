5bba3d0d90281583df25fbd342505ae67378cfe0
MarjanMeurisse <marjan.meurisse@hotmail.be>
2023-10-04 16:36:47 +0200

    Update docker

--- vaccine_effectiveness_analytical_pipeline/install.R
+++ vaccine_effectiveness_analytical_pipeline/install.R
@@ -1,11 +1,10 @@
 packages <- c("dplyr","arrow","validate","DataExplorer","DT","purrr","dlookr","survminer",
        "quarto","ggplot2","plotly","scales","formattable","naniar","duckdb","DBI","here",
        "grDevices","visdat","mice","tidyr","shiny","consort","parallel","MatchIt",
-       "survival","table1","tab","forestmodel","gtsummary","survRM2")
+       "survival","table1","tab","forestmodel","gtsummary","survRM2","log4R","xlsx","finalfit","dbplyr")
 
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
 if (any(installed_packages == FALSE)) {
-  install.packages(packages[!installed_packages])
+  install.packages(packages[!installed_packages],repos ="http://cran.us.r-project.org")
 }
-
